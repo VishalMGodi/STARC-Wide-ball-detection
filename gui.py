5792ed56a7718ec616d859da62b42336532b0751
RockerBot <rockerbotmail@gmail.com>
2023-08-12 13:39:11 +0530

    added progbar and mrkrs

--- gui.py
+++ gui.py
@@ -1,108 +1,6 @@
-# # import socket, cv2, pickle, struct, imutils
-# #
-# # # Socket Create
-# # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # host_name = socket.gethostname()
-# # host_ip = socket.gethostbyname(host_name)
-# # print('HOST IP:', host_ip)
-# # port = 9999
-# # socket_address = (host_ip, port)
-# #
-# # # Socket Bind
-# # server_socket.bind(socket_address)
-# #
-# # # Socket Listen
-# # server_socket.listen(5)
-# # print("LISTENING AT:", socket_address)
-# #
-# # # Socket Accept
-# # while True:
-# #     client_socket, addr = server_socket.accept()
-# #     print('GOT CONNECTION FROM:', addr)
-# #     if client_socket:
-# #         vid = cv2.VideoCapture(0)
-# #
-# #         while (vid.isOpened()):
-# #             img, frame = vid.read()
-# #             # frame = imutils.resize(frame, width=320)
-# #             a = pickle.dumps(frame)
-# #             message = struct.pack("Q", len(a)) + a
-# #             client_socket.sendall(message)
-# #
-# #             cv2.imshow('TRANSMITTING VIDEO', frame)
-# #             if cv2.waitKey(1) == '13':
-# #                 client_socket.close()
-#
-# import cv2
-# import threading
-# import tkinter as tk
-# from tkinter import ttk
-# from PIL import Image, ImageTk
-#
-#
-# class WebcamApp:
-#     def __init__(self, window, window_title):
-#         self.window = window
-#         self.window.title(window_title)
-#
-#         self.cap = cv2.VideoCapture(0)
-#         self.frames = []
-#
-#         self.video_label = tk.Label(window)
-#         self.video_label.pack()
-#
-#         self.record_button = tk.Button(window, text="Start Recording", command=self.toggle_record)
-#         self.record_button.pack()
-#
-#         self.slider = ttk.Scale(window, from_=0, to=0, orient="horizontal", command=self.update_frame)
-#         self.slider.pack()
-#
-#         self.update()
-#
-#     def toggle_record(self):
-#         if not self.frames:
-#             threading.Thread(target=self.record).start()
-#
-#     def record(self):
-#         while True:
-#             ret, frame = self.cap.read()
-#             if ret:
-#                 self.frames.append(frame.copy())
-#                 self.slider.config(to=len(self.frames) - 1)
-#                 # self.update_frame(len(self.frames) - 1)
-#                 # print(len(self.frames))
-#
-#     def update(self):
-#         if self.frames:
-#             frame = self.frames[-1]
-#             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#             img = Image.fromarray(frame)
-#             imgtk = ImageTk.PhotoImage(image=img)
-#             self.video_label.imgtk = imgtk
-#             self.video_label.config(image=imgtk)
-#
-#         self.window.after(10, self.update)
-#
-#     def update_frame(self, value):
-#         index = int(value)
-#         if 0 <= index < len(self.frames):
-#             frame = self.frames[index]
-#             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#             img = Image.fromarray(frame)
-#             imgtk = ImageTk.PhotoImage(image=img)
-#             self.video_label.imgtk = imgtk
-#             self.video_label.config(image=imgtk)
-#
-#
-# if __name__ == "__main__":
-#     root = tk.Tk()
-#     app = WebcamApp(root, "Webcam App")
-#     root.mainloop()
-
-# import streamlit
 import cv2
 import threading
-import sys
+import sys, time
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import QTimer
 from PIL import Image
@@ -111,46 +9,69 @@ import numpy as np
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Video codec
 output_filename1 = "VIEW1.mp4"
 output_filename2 = "VIEW2.mp4"
+datasetpath = "D:/DATA/GITHUB PROJECTS/STARC-Wide-ball-detection/Dataset/Dataset/"#"./Dataset/Dataset/"
 
+margin = 10
+vid_w = 640 #1920#640
+vid_h = 360 #1080#480
+btn_w = 120
+btn_h = 30
+slider_width = vid_w+margin+vid_w
+clr_red = QtGui.QColor("red")
+clr_green = QtGui.QColor("green")
 
+def randomFunc(self):
+    pass
 class WebcamApp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.cap = cv2.VideoCapture(0)
-        self.frames1 = []
+        # self.cap = cv2.VideoCapture(0)
+        self.cap_main = cv2.VideoCapture(datasetpath+"New_5_MainView.mp4")
+        self.cap_bat = cv2.VideoCapture(datasetpath+"New_5_BatView.mp4")
+        # self.cap_bat = self.cap_main = cv2.VideoCapture(0)
+        self.frames1 = []#(1080, 1920, 3) (1080, 1920, 3)
         self.frames2 = []
-        self.clip = [0,0]
+        self.clip = [-1,-1]
         self.is_recording = False
 
         self.video_label1 = QtWidgets.QLabel(self)
-        self.video_label1.setGeometry(10, 10, 640, 480)
+        self.video_label1.setGeometry(margin, margin, vid_w, vid_h) # x, y, w, h
 
         self.video_label2 = QtWidgets.QLabel(self)
-        self.video_label2.setGeometry(660, 10, 640, 480)
+        self.video_label2.setGeometry(margin+vid_w+margin, margin, vid_w, vid_h)
 
-        self.record_button = QtWidgets.QPushButton("Start Recording", self)
-        self.record_button.setGeometry(10, 500, 120, 30)
+        self.record_button = QtWidgets.QPushButton("Clip", self)
+        self.record_button.setGeometry(margin, margin+vid_h+margin, btn_w, btn_h)
         self.record_button.clicked.connect(self.toggle_record)
 
         self.save_button = QtWidgets.QPushButton("Predict (Save)", self)
-        self.save_button.setGeometry(500, 500, 120, 30)
+        self.save_button.setGeometry(500, margin+vid_h+margin, btn_w, btn_h)
         self.save_button.clicked.connect(self.video)
         self.save_button.hide()
 
         self.mrk_strt_pt_button = QtWidgets.QPushButton("Mark as Start", self)
-        self.mrk_strt_pt_button.setGeometry(200, 500, 120, 30)
+        self.mrk_strt_pt_button.setGeometry(200, margin+vid_h+margin, btn_w, btn_h)
         self.mrk_strt_pt_button.clicked.connect(self.mark_point_start)
         self.mrk_strt_pt_button.hide()
 
         self.mrk_end_pt_button = QtWidgets.QPushButton("Mark as End", self)
-        self.mrk_end_pt_button.setGeometry(350, 500, 120, 30)
+        self.mrk_end_pt_button.setGeometry(350, margin+vid_h+margin, btn_w, btn_h)
         self.mrk_end_pt_button.clicked.connect(self.mark_point_end)
         self.mrk_end_pt_button.hide()
 
         self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)  # Use QtCore.Qt.Horizontal
-        self.slider.setGeometry(10, 550, 1280, 30)
+        self.slider.setGeometry(margin, margin+vid_h+margin+margin+btn_h+margin, slider_width, btn_h)
         self.slider.valueChanged.connect(self.update_frame)
+
+        self.progress_bar = QtWidgets.QProgressBar(self)
+        self.progress_bar.setGeometry(margin +btn_w +margin, margin+vid_h+margin, slider_width - margin-margin-btn_w, btn_h)
+        self.progress_bar.setValue(0)
+        self.progress_bar.hide()
+
+        self.label = QtWidgets.QLabel("Predicted", self)  # wide? or not?
+        self.label.setGeometry(margin +btn_w +margin, margin+vid_h+margin, 380, btn_h)
+        self.label.hide()
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update)
@@ -161,44 +82,46 @@ class WebcamApp(QtWidgets.QWidget):
     def toggle_record(self):
         if not self.is_recording:
             self.is_recording = True
-            self.record_button.setText("Stop Recording")
+            self.record_button.setText("Follow Live")
             self.mrk_strt_pt_button.show()
             self.mrk_end_pt_button.show()
             # self.save_button.show()
             # self.frames = []
         else:
             self.is_recording = False
-            self.record_button.setText("Start Recording")
+            self.record_button.setText("Clip")
             self.mrk_strt_pt_button.hide()
             self.mrk_end_pt_button.hide()
             self.save_button.hide()
-            self.clip = [0, 0]
+            self.clip = [-1, -1]
 
     def record(self):
         # while self.is_recording:
         while True:
-            ret1, frame1 = self.cap.read()#gets new frame
-            ret2, frame2 = self.cap.read()  # gets new frame
+            ret1, frame1 = self.cap_main.read() # gets new frame
+            ret2, frame2 = self.cap_bat.read() # gets new frame
             if ret1 and ret2:
+                # print(frame1.shape, frame2.shape)
                 max_frames = len(self.frames1) - 1
                 self.frames1.append(frame1.copy())#adds it
                 self.frames2.append(frame2.copy())#adds it
 
                 if not self.is_recording:
-                    self.slider.setMaximum(len(self.frames1) - 1)
-                    self.slider.setValue(len(self.frames1) - 1)
-                    self.update_frame(len(self.frames1) - 1)
+                    self.slider.setMaximum(max_frames)
+                    self.slider.setValue(max_frames)
+                    self.update_frame(max_frames)
                 # else: self.slider.setValue(min(self.slider.value()+1)
 
     def update(self):
         if self.frames1 and self.frames2:
-            # frame = cv2.cvtColor(self.frames[-1], cv2.COLOR_BGR2RGB)
             # print(self.slider.value())
+            ndx = self.slider.value()
+            cv2.resize(self.frames1[ndx], (640, 360))
             image1 = Image.fromarray(
-                cv2.cvtColor(self.frames1[self.slider.value()], cv2.COLOR_BGR2RGB)
+                cv2.cvtColor(cv2.resize(self.frames1[ndx], (640, 360)), cv2.COLOR_BGR2RGB)
             )
             image2 = Image.fromarray(
-                cv2.cvtColor(self.frames2[self.slider.value()], cv2.COLOR_BGR2RGB)
+                cv2.cvtColor(cv2.resize(self.frames2[ndx], (640, 360)), cv2.COLOR_BGR2RGB)
             )
             self.video_label1.setPixmap(
                 QtGui.QPixmap.fromImage(
@@ -211,14 +134,33 @@ class WebcamApp(QtWidgets.QWidget):
                 )
             )
 
+    def paintEvent(self, event):
+        print("called")
+        max_val = self.slider.maximum()
+        painter = QtGui.QPainter(self)
+        painter.setPen(QtGui.QPen(clr_green, 5, QtCore.Qt.SolidLine))
+        if self.clip[0] != -1:
+            s_val = int((self.clip[0] / max_val) * slider_width) + margin
+            painter.drawLine(
+                QtCore.QPoint(s_val, margin + vid_h + margin + margin + btn_h + margin),
+                QtCore.QPoint(s_val, margin + vid_h + margin + margin + btn_h + margin + 50)
+            )
+        painter.setPen(QtGui.QPen(clr_red, 5, QtCore.Qt.SolidLine))
+        if self.clip[1] != -1:
+            e_val = int((self.clip[1] / max_val) * slider_width) + margin
+            painter.drawLine(
+                QtCore.QPoint(e_val, margin + vid_h + margin + margin + btn_h + margin),
+                QtCore.QPoint(e_val, margin + vid_h + margin + margin + btn_h + margin + 50)
+            )
+
     def update_frame(self, value):
         index = int(value)
         if 0 <= index < len(self.frames1) <= len(self.frames2):
             image1 = Image.fromarray(
-                cv2.cvtColor(self.frames1[index], cv2.COLOR_BGR2RGB)
+                cv2.cvtColor(cv2.resize(self.frames1[index], (640, 360)), cv2.COLOR_BGR2RGB)
             )
             image2 = Image.fromarray(
-                cv2.cvtColor(self.frames2[index], cv2.COLOR_BGR2RGB)
+                cv2.cvtColor(cv2.resize(self.frames2[index], (640, 360)), cv2.COLOR_BGR2RGB)
             )
             self.video_label1.setPixmap(
                 QtGui.QPixmap.fromImage(
@@ -261,6 +203,13 @@ class WebcamApp(QtWidgets.QWidget):
         out2.release() # Release the VideoWriter object and close the video file
 
         print("Videos saved as", output_filename1,"and", output_filename2)
+        self.progress_bar.show()
+        i=0
+        while i<100:
+            time.sleep(0.1)
+            i+=5 # TODO
+            self.progress_bar.setValue(i)
+        self.progress_bar.hide()
 
 
 if __name__ == "__main__":
